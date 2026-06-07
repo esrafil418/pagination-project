a604c387b28964f86887cd2a25ef2efa5be39484
Esrafil <esrafil.s.r.man418@gmail.com>
2026-06-07 16:39:15 +0330

    feat: Creat a Table for todos

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,51 @@
+import { useEffect, useState } from "react";
+
 export default function App() {
-  return <div>App</div>;
+  const [todos, setTodos] = useState([]);
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/todos")
+      .then((res) => res.json())
+      .then((datas) => {
+        setTodos(datas);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+  return (
+    <div>
+      {!todos ? (
+        "Loading"
+      ) : (
+        <table className="table">
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>User ID</th>
+              <th>Title</th>
+              <th>Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            {todos.map((todo) => (
+              <tr>
+                {console.log(todo)}
+                <td>{todo.id}</td>
+                <td>{todo.userId}</td>
+                <td>{todo.title}</td>
+                <td>
+                  <p
+                    className={
+                      todo.completed ? "btn btn-success" : "btn btn-danger"
+                    }
+                  >
+                    {todo.completed ? "Completed" : "Pending"}
+                  </p>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      )}
+    </div>
+  );
 }
